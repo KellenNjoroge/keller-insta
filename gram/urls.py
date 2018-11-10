252ae462c6b44e50f392ca3478a9737d1d7b81cd
KellenNjoroge <muthonkel@gmail.com>
2018-11-10 18:22:38 +0300

    profile generally works

--- gram/urls.py
+++ gram/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url('profile/$',views.profile,name='profile'),
+    # url('^explore/',views.explore_results,name='explore'),
+    # url('^update/$',views.update,name='update'),
+    # url('^new_post/',views.new_post,name='new_post'),
+    # url('^comment/(\d+)',views.comment,name='comment')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- gram/views.py
+++ gram/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse,Http404
+from .models import *
+from django.contrib.auth.decorators import login_required
+# from .forms import *
+from django.db import transaction
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to Keller Insta')
+    return render(request,"index.html")
+
+
+# @login_required(login_url='/accounts/login/')
+def profile(request):
+    current_user = request.user
+    profile = Profile.objects.get(user=current_user)
+    print(profile)
+    # profile = Profile.objects.filter(user=request.user.id)
+    # images = Image.objects.filter(profile = current_user)
+
+    return render(request,'profile.html',{'profile':profile})
