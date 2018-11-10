73ba9fead13baee591cf25a17f1317ce3f418a08
KellenNjoroge <muthonkel@gmail.com>
2018-11-10 18:26:02 +0300

    image and comment models and forms

--- gram/models.py
+++ gram/models.py
@@ -12,20 +12,59 @@ class Profile(models.Model):
     # username = models.CharField(max_length=25,unique=True)
     # relate = models.ManyToManyField('self', symmetrical=False, through='Relationship')
     bio = models.TextField(max_length=100, blank=True)
-    profilepic = models.ImageField(upload_to='picture/',default=True)
+    profilepic = models.ImageField(upload_to='picture/', default=True)
     # email = models.EmailField()
-    contact = models.CharField(max_length=15,blank=True)
+    contact = models.CharField(max_length=15, blank=True)
 
-
-
-    @receiver(post_save,sender=User)
-    def create_user_profile(sender,instance,created,**kwargs):
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
 
-    @receiver(post_save,sender=User)
-    def save_user_profile(sender,instance,**kwargs):
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
 
     def __str__(self):
         return self.user.username
+
+
+class Image(models.Model):
+    image = models.CharField(max_length=40)
+    caption = models.CharField(max_length=100)
+    path = models.ImageField(upload_to='picture/', default=True)
+    profile = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_image(cls, id):
+        image = cls.objects.get(id=id)
+        return image
+
+    @classmethod
+    def getImages(cls):
+        images = cls.objects.all()
+        return images
+
+    @classmethod
+    def image_comments(cls, id):
+        comments = cls.objects.filter(imagecomments__comment_id=id)
+        posters = cls.objects.filter(imagecomments__commnetator__id=id)
+        return comments, posters
+
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=100)
+    commentator = models.ForeignKey(User)
+    comment_image = models.ForeignKey(Image, related_name='comment', null=True)
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
